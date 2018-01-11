e935663e141f09b3a07f395feb894507c85610aa
ryteone1 <rynlacy@yahoo.com>
2018-01-11 09:00:12 -0500

    major progress on comparison page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -4,6 +4,23 @@ import React, { Component } from 'react'
 // https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
 import { Link } from "react-router";
 
+const styles = {
+  font: {
+    color: "blue"
+  },
+
+  image: {
+    width: 200
+  },
+
+  column: {
+    padding: 0
+  }
+};
+
+
+
+
 // Create the Main component
 class Main extends Component {
 
@@ -28,42 +45,108 @@ class Main extends Component {
                   <span className="icon-bar"></span>
                   <span className="icon-bar"></span>
                 </button>
-                <Link className="navbar-brand" to="/">Ryte Car-Comparisons</Link>
+                <Link className="navbar-brand" to="/">Ryte-Auto  Comparisons</Link>
               </div>
 
               <div className="collapse navbar-collapse navbar-ex1-collapse">
                 <ul className="nav navbar-nav navbar-right">
                   {/* Using <Link> in place of <a> and "to" in place of "href" */}
-                  <li><Link to="/search">Search</Link></li>
-                  <li><Link to="/saved">Saved Articles</Link></li>
+                  <li><Link to="/search">Cars for Sale</Link></li>
+                  <li><Link to="/saved">Research</Link></li>
                 </ul>
               </div>
             </div>
           </nav>
 
-          {/* Jumbotron */}
-          <div className="jumbotron">
-            <h2 className="text-center"><strong>(ReactJS) New York Times Article Scrubber</strong></h2>
-            <h3 className="text-center">Search for and save articles of interest.</h3>
-          </div>
-
           <div className="container">
-              <h1>Hello</h1>
 
-          </div>
+            <div className="row">
 
-          <div className="row">
-             <div className="col-sm-6 col-md-4">
-                <div className  ="thumbnail">
-                  <img src="..." alt="..." />
-                  <div className  ="caption">
-                    <h3>Thumbnail label</h3>
-                    <p>...</p>
-                    <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-                 </div>
+                <div className="col-sm-2 col-md-2">
+                  <p>Comparing 1-4 of 4</p>
                 </div>
-              </div>
+
+                <div className="col-sm-2 col-md-2">
+                  <a href="#" className="btn btn-primary btn-xs" role="button">Change</a> <a href="#" className="btn btn-default btn-xs" role="button">Remove</a>
+                </div>
+
+                <div className="col-sm-2 col-md-2">
+                  <a href="#" className="btn btn-primary btn-xs" role="button">Change</a> <a href="#" className="btn btn-default btn-xs" role="button">Remove</a>
+                </div>
+
+                <div className="col-sm-2 col-md-2">
+                  <a href="#" className="btn btn-primary btn-xs" role="button">Change</a> <a href="#" className="btn btn-default btn-xs" role="button">Remove</a>
+                </div>
+
+                <div className="col-sm-2 col-md-2">
+                  <a href="#" className="btn btn-primary btn-xs" role="button">Change</a> <a href="#" className="btn btn-default btn-xs" role="button">Remove</a>
+                </div>
+
+            </div>
+
+            <div className="row"> 
+
+                <div style={styles.column} className="col-sm-2 col-md-2">
+                  <div className="thumbnail">                    
+                    <div className  ="caption">
+                      <a href="#" className="btn btn-primary btn-xs" role="button">New Comparison</a>
+                      
+                   </div>
+                  </div>
+                </div>     
+
+                <div style={styles.column} className="col-sm-2 col-md-2">
+                  <div className="thumbnail">
+                    <img src="http://images.nadaguides.com/ChromeImageGallery/Expanded/Transparent/320/2014FRD007a_320/2014FRD007a_320_01.png" alt="Ford Explorer" />
+                    <div className  ="caption">
+                      <h3 style={styles.font}>2014 GMC Acadia</h3>
+                      <p>...</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                   </div>
+                  </div>
+                </div>         
+
+              
+                <div style={styles.column} className="col-sm-2 col-md-2">
+                  <div className="thumbnail">
+                    <img src="http://images.nadaguides.com/ChromeImageGallery/Expanded/Transparent/320/2014HON011b_320/2014HON011b_320_01.png" />
+                    <div className  ="caption">
+                      <h3>2014 Honda Pilot</h3>
+                      <p>...</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                   </div>
+                  </div>
+                </div>
+             
+              
+                <div style={styles.column} className="col-sm-2 col-md-2">
+                  <div className="thumbnail">
+                    <img src="http://images.nadaguides.com/ChromeImageGallery/Expanded/Transparent/320/2014NIS008a_320/2014NIS008a_320_01.png" alt="GMC Acadia" />
+                    <div className  ="caption">
+                      <h3>2014 Nissan Pathfinder</h3>
+                      <p>...</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                   </div>
+                  </div>
+                </div>             
+
+            
+                <div style={styles.column} className="col-sm-2 col-md-2">
+                  <div className="thumbnail">
+                    <img src="http://images.nadaguides.com/ChromeImageGallery/Expanded/Transparent/320/2014CHE019a_320/2014CHE019a_320_01.png" alt="Chevy Traverse" />
+                    <div className  ="caption">
+                      <h3>2014 Chevy Traverse</h3>
+                      <p>...</p>
+                      <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                   </div>
+                  </div>
+                </div>
+             
+            </div>  
+
           </div>
+
+          
 
 
           {/* Here we will deploy the sub components (Search or Saved */}
